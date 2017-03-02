9ada93c7936b37b7fc422256674b9b6a5ff66f1c
vadim <nikulkin_vadim98@mail.ru>
2017-03-02 21:17:06 +0300

    hello

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <Windows.h>
+#define N 100
+
+void print1()
+{
+    printf("1");
+}
+
+void print2()
+{
+    printf("2");
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    while(1)
+    {
+        char answer[N];
+        gets(answer);
+        if(answer[0] == 'ä')
+            print1();
+        else if(answer[0] == 'í')
+            print2();
+        printf("\n");
+    }
+
+   // char s[N];
+    //gets(s);
+   // printf("\n\n");
+    //shifr(s);
+    return 0;
+}
